f8ac4052019071aa167c9f9747a717638c9ae211
Sanjana-Boligorla <130239451+Sanjana-Boligorla@users.noreply.github.com>
2024-04-23 09:22:39 +0530

    Update viewinfo.js

--- src/components/viewinfo.js
+++ src/components/viewinfo.js
@@ -6,14 +6,15 @@ import { faCcPaypal, faGooglePay, faApplePay, faAmazonPay, faPaypal } from '@for
 import Header from './header'
 import Footer from './footer'
 
-import './bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import icon1 from './icon1.jpg'
 import logo from './logo1.jpg';
 import icon2 from './icon2.jpg'
 import icon6 from './icon6.png'
 import icon5 from './icon5.png'
 import comp2 from './comp2.png'
-import { Carousel } from 'bootstrap';
+
 import car from './car3-Photoroom.png-Photoroom.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
